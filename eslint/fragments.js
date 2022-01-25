fc97657b39ee1a34323ad62ccca3c33fc9d38cf0
Julian Waller <julian@superfly.tv>
2022-01-25 13:54:54 +0000

    feat: enable strict type checking. enable some more rules (#8)

--- eslint/fragments.js
+++ eslint/fragments.js
@@ -12,6 +12,7 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 		'plugin:node/recommended',
 		'plugin:jest/recommended',
 		'prettier',
@@ -33,6 +34,19 @@ module.exports = {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_(.+)' }],
 		'@typescript-eslint/no-floating-promises': 'error',
+		'@typescript-eslint/explicit-module-boundary-types': ['error'],
+		'@typescript-eslint/promise-function-async': 'error',
+		'@typescript-eslint/require-await': 'off', // conflicts with 'promise-function-async'
+
+		/** Disable some annoyingly strict rules from the 'recommended-requiring-type-checking' pack */
+		'@typescript-eslint/no-unsafe-assignment': 0,
+		'@typescript-eslint/no-unsafe-member-access': 0,
+		'@typescript-eslint/no-unsafe-argument': 0,
+		'@typescript-eslint/no-unsafe-return': 0,
+		'@typescript-eslint/no-unsafe-call': 0,
+		'@typescript-eslint/restrict-template-expressions': 0,
+		'@typescript-eslint/restrict-plus-operands': 0,
+		/** End 'recommended-requiring-type-checking' overrides */
 	},
 
 	tsParser: {
